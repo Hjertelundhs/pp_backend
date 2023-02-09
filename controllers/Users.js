3c94831861457cc5dfa88c8b881e155f2c6bf9fd
Hjertelundhs <tobias.hjertelundh@hotmail.com>
2023-02-09 13:43:38 +0100

    Adding users and products

---
+++ controllers/Users.js
@@ -0,0 +1,8 @@
+import User from '../models/UserModel'
+
+export const getUsers = (req, res) => {}
+
+export const getUserById = (req, res) => {}
+export const createUser = (req, res) => {}
+export const updateUser = (req, res) => {}
+export const deleteUser = (req, res) => {}
---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import session from 'express-session'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: 'auto',
+    },
+  })
+)
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  })
+)
+app.use(express.json)
+
+const port = process.env.APP_PORT
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
